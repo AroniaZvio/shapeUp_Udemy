8fe47205d17a2813a07814a73c4949bb9a1b3626
AroniaZvio <aroniazvio@gmail.com>
2024-03-02 15:27:38 +0400

    main

--- src/Screens/WorkoutScreen.jsx
+++ src/Screens/WorkoutScreen.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import Welcome from '../Components/Welcome';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import WorkoutOTD from "../Components/WorkoutOTD"; 
+import Separator from '../Components/Separator';
 
 function WorkoutScreen() {
   return (
     <SafeAreaView>
       <Welcome />
       <WorkoutOTD />
+      <Separator />
     </SafeAreaView>
   );
 }
