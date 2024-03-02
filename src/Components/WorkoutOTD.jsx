e7ca27e374a2443f162f425c3f7affe8aaa1d25f
AroniaZvio <aroniazvio@gmail.com>
2024-03-02 14:23:03 +0400

    main

--- src/Components/WorkoutOTD.jsx
+++ src/Components/WorkoutOTD.jsx
@@ -22,7 +22,7 @@ const WorkoutOTD = () => {
           resizeMode="cover"
         >
           <View style={styles.textContainer}>
-            <Text style={styles.text}>Workout of Day 900</Text>
+            <Text style={styles.text}>Workout of Day 500</Text>
           </View>
         </ImageBackground>
       </View>
