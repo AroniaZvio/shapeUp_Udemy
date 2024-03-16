bb9a41182983345343c9bb016dcf22eb129adf5c
AroniaZvio <aroniazvio@gmail.com>
2024-03-16 11:56:53 +0400

    main

--- src/Components/WorkoutOTD.jsx
+++ src/Components/WorkoutOTD.jsx
@@ -28,7 +28,7 @@ const WorkoutOTD = () => {
               className="text-white/70 text-3xl tracking-tighter"
               style={{ fontFamily: "Lato_400Regular" }}
             >
-              Workout Of The Day 221
+              Workout Of The Day 03_16_2024
             </Text>
           </View>
         </ImageBackground>
